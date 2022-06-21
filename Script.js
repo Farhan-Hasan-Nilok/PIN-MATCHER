c799cdba610cee066ed7c4cd927de168429628ec
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-06-21 16:54:30 +0600

    complete

--- Script.js
+++ Script.js
@@ -13,4 +13,34 @@ function generatePin(){
 document.getElementById("click-btn").addEventListener("click", function(){
     const pin = generatePin();
     document.getElementById("input-value").value = pin;
+});
+
+document.getElementById("key-pad").addEventListener("click", function(event){
+    const number = event.target.innerText;
+    const keyPadInput = document.getElementById("num-input");
+
+    if(isNaN(number)){
+      if (number == "C") {
+        keyPadInput.value = "";
+      }
+    }
+
+    else{
+      const firstInput = keyPadInput.value;
+      const newInput = firstInput + number;
+      keyPadInput.value = newInput;
+    }
 })
+
+function verifyPin(){
+  const checkPin = document.getElementById("input-value").value;
+  const keyPadInput = document.getElementById("num-input").value;
+
+  if(checkPin == keyPadInput){
+    document.getElementById("notify-tick").style.display = "block";
+  }
+
+  else{
+    document.getElementById("notify-cross").style.display = "block"
+  }
+}
